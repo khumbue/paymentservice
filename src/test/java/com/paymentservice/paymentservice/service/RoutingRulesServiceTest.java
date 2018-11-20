4d11388425d2e1a5d17997f79ac70bf4aeb66851
khumbue <kncube68@yahoo.com>
2018-11-20 07:15:42 +0200

    removed db test and moved logic to repository test

--- src/test/java/com/paymentservice/paymentservice/service/RoutingRulesServiceTest.java
+++ src/test/java/com/paymentservice/paymentservice/service/RoutingRulesServiceTest.java
@@ -1,12 +1,39 @@
 package com.paymentservice.paymentservice.service;
 
+import com.paymentservice.paymentservice.PaymentserviceApplication;
+import com.paymentservice.paymentservice.model.RoutingRule;
+import com.paymentservice.paymentservice.repository.RoutingRuleRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PaymentserviceApplication.class)
 public class RoutingRulesServiceTest {
 
+    @Autowired
+    private RoutingRuleRepository routingRuleRepository;
+
     @Test
-    public void getRoutingRules() {
+    public void givenGenericEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
+        RoutingRule routingRule = generateRoutingRule();
+        routingRule = routingRuleRepository.save(routingRule);
+
+        RoutingRule foundRoutingRule = routingRuleRepository.findById(routingRule.getId()).get();
+
+        assertNotNull(foundRoutingRule);
+        assertEquals(routingRule.getSettlementEngine(), foundRoutingRule.getSettlementEngine());
+    }
+
+    private RoutingRule generateRoutingRule() {
+        RoutingRule routingRule = new RoutingRule();
+        routingRule.setId(1003L);
+        routingRule.setSettlementEngine("dummySettlementEngine");
+        return routingRule;
     }
 }
